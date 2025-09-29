6e6bcd5171daed85185558ea01248558efd69b61
Gustavo <gustavo_bonifacio2020@outlook.com>
2025-09-29 10:12:06 -0300

    update(Feature/TransactionTest)

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -5,16 +5,48 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
+use App\Models\Transaction;
 
-class TransactionTest
+class TransactionTest extends TestCase
 {
+    private array $pessoal_information_to_test = [
+        'name' => 'Fulano de Tal',
+        'email' => 'fulano@exemplo.com',
+        'password' => 'senhaSegura123',
+        'confirm_password' => 'senhaSegura123'
+    ];
+
+    private string $access_token;
+
+    private function login()
+    {
+        $response = $this->postJson('api/auth/login', [
+            'name' => $this->pessoal_information_to_test['name'],
+            'email' => $this->pessoal_information_to_test['email'],
+            'password' => $this->pessoal_information_to_test['password'],
+            'confirm_password' => $this->pessoal_information_to_test['confirm_password'],
+        ]);
+        $response->assertStatus(200)->assertJsonStructure(['access_token', 'token_type', 'expires_in']);
+
+        $this->access_token = $response['access_token'];
+    }
+
     #[Test]
     public function test_initialization()
     {
         //test initialization
+        $this->login();
+        $this->deposit();
     }
 
     public function deposit(){
-        $response = $this->postJson('api/auth/register', $this->pessoal_information_to_test);
+        $args = [
+            'type' => Transaction::TYPE_DEPOSIT,
+            'amount' => "150.50",
+            // 'scheduled_at' => now()->addDays(1)->format('m-d-Y')
+        ];
+
+        $response = $this->post('api/transactions/', $args, ['Authorization' => $this->access_token]);
+        $data = json_decode($response->getContent(), true);
     }
 }
