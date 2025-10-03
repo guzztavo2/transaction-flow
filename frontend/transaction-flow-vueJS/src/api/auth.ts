ace2f6c0494eaa8d42f3f7f6cace3c8f6387f49c
Gustavo <gustavo_bonifacio2020@outlook.com>
2025-10-03 18:47:32 -0300

    feat: add main application structure with navigation and router view components

---
+++ frontend/transaction-flow-vueJS/src/api/auth.ts
@@ -0,0 +1,16 @@
+import { api } from './http'
+import { User } from '@/types/user'
+
+export async function login(email: string, password: string, remember: boolean = false): Promise<User> {
+    try {
+        const response = await api.post('/login', { email, password, remember })
+        const userData = response.data.user
+        return new User(userData.name, userData.email);
+    } catch (error: unknown) {
+        if (typeof error === 'object' && error !== null && 'response' in error) {
+            const err = error as { response?: { data?: { message?: string } } };
+            throw new Error(err.response?.data?.message || 'Login failed');
+        }
+        throw new Error('Login failed');
+    }
+}
---
+++ frontend/transaction-flow-vueJS/src/components/routerView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineAsyncComponent, computed } from 'vue';
+import { resolveRoute } from '../routes/index';
+
+const view = computed(() => defineAsyncComponent(() => resolveRoute()['component']()));
+console.log(view);
+</script>
+
+<template>
+    <component :is="view" />
+</template>
