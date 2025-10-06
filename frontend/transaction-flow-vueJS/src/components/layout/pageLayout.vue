c941098036fd0b09b0efebdd05422cbfcb14e626
Gustavo <gustavo_bonifacio2020@outlook.com>
2025-10-06 20:23:35 -0300

    update(layout/PageLayout)

--- frontend/transaction-flow-vueJS/src/components/layout/pageLayout.vue
+++ frontend/transaction-flow-vueJS/src/components/layout/pageLayout.vue
@@ -1,14 +1,12 @@
 <template>
     <div class="flex flex-col w-full h-full justify-content-center items-center flex-nowrap py-2">
-
-        <div class="flex flex-row w-full flex-1 flex-nowrap p-2">
-            <div class="divider divider-horizontal"></div>
+        <div class="flex flex-row w-full max-w-6xl flex-1 flex-nowrap">
+            <div class="divider divider-horizontal hidden sm:block"></div>
             <div class="flex flex-col w-full flex-1 flex-nowrap">
                 <slot></slot>
             </div>
-            <div class="divider divider-horizontal"></div>
+            <div class="divider divider-horizontal hidden sm:block"></div>
         </div>
-
     </div>
 </template>
 
