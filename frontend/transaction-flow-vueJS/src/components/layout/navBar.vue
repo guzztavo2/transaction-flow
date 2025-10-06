9702aabff2fa452e9f117345130f98c755ece1b7
Gustavo <gustavo_bonifacio2020@outlook.com>
2025-10-06 20:22:28 -0300

    update(NavBarComponent)

--- frontend/transaction-flow-vueJS/src/components/layout/navBar.vue
+++ frontend/transaction-flow-vueJS/src/components/layout/navBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar bg-primary text-primary-content rounded shadow-lg">
+  <div class="navbar bg-primary text-primary-content p-4 rounded shadow-lg">
     <div class="navbar-start">
       <div class="dropdown">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
@@ -9,9 +9,22 @@
         </div>
         <ul tabindex="0"
           class="menu bg-primary border-2 border-solid border-info menu-sm dropdown-content rounded-box z-1 mt-3 w-52 p-4 text-4xl font-bold shadow">
-          <li><a class="block text-center">Login</a></li>
-          <li><a class="block text-center">Register</a></li>
-          <li><a class="block text-center">About</a></li>
+
+          <li v-for="(value, index) in contentMenu[languageStore.language as Language]" :key="value">
+            <template v-if="index == 0">
+              <a class="block text-center" href="/">{{ value }}</a>
+            </template>
+            <template v-if="index == 1">
+              <a class="block text-center" href="/about">{{ value }}</a>
+            </template>
+            <template v-if="index == 2">
+              <a class="block text-center" href="/login">{{ value }}</a>
+            </template>
+            <template v-if="index == 3">
+              <a class="block text-center" href="/register">{{ value }}</a>
+            </template>
+          </li>
+
         </ul>
       </div>
     </div>
@@ -19,20 +32,33 @@
       <a href="/" class="btn btn-ghost text-xl">TransactionFLOW</a>
     </div>
     <div class="navbar-end">
-      <label class="flex items-center gap-2 cursor-pointer">
-        <input type="checkbox" class="toggle theme-controller" :checked="theme === 'dark'"
-          @change="theme = theme === 'light' ? 'dark' : 'light'" />
-        <span>{{ theme === 'light' ? '🌞' : '🌙' }}</span> </label>
+      <div class="flex flex-row flex-wrap align-center justify-center gap-4">
+        <label class="flex items-center gap-2 cursor-pointer">
+          <input type="checkbox" class="toggle theme-controller" :checked="theme === 'dark'"
+            @change="theme = theme === 'light' ? 'dark' : 'light'" />
+          <span class="shadow block bg-neutral text-xl p-2 rounded">{{ theme === 'light' ? '🌞' : '🌙' }}</span>
+        </label>
+        <label class="flex items-center gap-2 cursor-pointer">
+          <input type="checkbox" class="toggle theme-controller" :checked="languageStore.language === 'BR'"
+            @change="languageStore.toggleLanguage()" />
+          <span class="shadow block bg-neutral text-xl p-2 rounded">{{ languageStore.language === 'BR' ? '🇧🇷' :
+            '🇺🇸'
+          }}</span>
+        </label>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, watch } from "vue";
+import { useLanguageStore, type Language } from "@/stores/language";
 
 const theme = ref('dark');
 
-onMounted(() => {
+const languageStore = useLanguageStore();
+
+function handleChangeTheme() {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     theme.value = savedTheme;
@@ -43,5 +69,15 @@ onMounted(() => {
     localStorage.setItem("theme", newTheme);
     document.documentElement.setAttribute("data-theme", newTheme);
   });
-})
+}
+
+onMounted(() => {
+  handleChangeTheme();
+});
+
+const contentMenu: Record<Language, string[]> = {
+  "BR": ["Início", "Sobre", "Entrar", "Registrar"],
+  "EN": ["Home", "About", "Login", "Register",]
+}
+
 </script>
