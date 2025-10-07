c076d81d8fd947358e8c46c5aa6e592b652c36fe
Gustavo <gustavo_bonifacio2020@outlook.com>
2025-10-07 17:32:29 -0300

    update(carouselComponent)

--- frontend/transaction-flow-vueJS/src/components/layout/caroulselItem.vue
+++ frontend/transaction-flow-vueJS/src/components/layout/caroulselItem.vue
@@ -1,17 +1,19 @@
 <template>
     <div :id="item.id" class="carousel-item relative w-full">
         <div class="card bg-primary image-full w-full shadow-sm">
-            <figure class="w-full">
-                <img :src="item.image" :alt="item.alt" />
+            <figure class="w-full p-2">
+                <img class="w-full opacity-75" :src="item.image" :alt="item.alt" />
             </figure>
-            <div class="card-body m-12">
+
+            <div class="card-body p-5 m-5 min-h-[400px] sm:min-h-0 md:m-10">
                 <h2 class="card-title rounded shadow-sm border-b p-2 inline text-2xl text-center">
                     {{ item.title }}
                 </h2>
-                <div class="flex h-full w-full flex flex-col">
-                    <p v-for="(text, index) in item.texts" :key="index" class="text-xl p-5 text-end">
-                        {{ text }}
-                    </p>
+
+                <div class="flex h-full w-full flex flex-col items-center justify-center p-4">
+                    <div class="w-full h-full">
+                        <slot></slot>
+                    </div>
                 </div>
             </div>
         </div>
@@ -25,9 +27,5 @@
 
 <script setup lang="ts">
 import type { CarouselItem as CarouselItemType } from '@/types/CarouselItem';
-import { onMounted } from 'vue';
-onMounted(() => {
-    alert("Montado");
-});
 defineProps<{ item: CarouselItemType }>()
 </script>
