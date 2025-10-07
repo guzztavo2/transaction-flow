1769f052db9e50d0295ac72ca8adb1a6fb2496f2
Gustavo <gustavo_bonifacio2020@outlook.com>
2025-10-07 17:34:16 -0300

    feat(footer in pageLayout)

--- frontend/transaction-flow-vueJS/src/components/layout/pageLayout.vue
+++ frontend/transaction-flow-vueJS/src/components/layout/pageLayout.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="flex flex-col w-full h-full justify-content-center items-center flex-nowrap py-2">
         <div class="flex flex-row w-full max-w-6xl flex-1 flex-nowrap">
-            <div class="divider divider-horizontal hidden sm:block"></div>
+            <div class="divider divider-horizontal hidden sm:flex"></div>
             <div class="flex flex-col w-full flex-1 flex-nowrap">
                 <slot></slot>
+                <footerBar></footerBar>
             </div>
-            <div class="divider divider-horizontal hidden sm:block"></div>
+            <div class="divider divider-horizontal hidden sm:flex"></div>
         </div>
     </div>
 </template>
 
 
 <script setup lang="ts">
-
+import footerBar from './footerBar.vue';
 </script>
